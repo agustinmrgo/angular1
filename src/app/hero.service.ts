30e9fa5e3819dfb2386b2df419fee7dbdb1b2d35
agustinmrgo <agustinmrgo@gmail.com>
2018-04-01 18:56:41 -0300

    Servicios terminado

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+
 import { Observable } from 'rxjs/observable';
 import { of } from 'rxjs/observable/of';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes (): Observable<Hero[]> {
-      return of(HEROES);
+    // mensaje enviado despues de obtener los heroes de la bd (?
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
-
 }
